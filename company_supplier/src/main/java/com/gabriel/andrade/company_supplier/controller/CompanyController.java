02640d71f7e4d8696aa1dcf2592a6772b2c35e6e
Gabriel Silva Andrade <gagabrielsilva007@gmail.com>
2023-03-21 22:01:31 -0300

    Ajuestes end point

--- company_supplier/src/main/java/com/gabriel/andrade/company_supplier/controller/CompanyController.java
+++ company_supplier/src/main/java/com/gabriel/andrade/company_supplier/controller/CompanyController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/company")
@@ -26,9 +27,14 @@ public class CompanyController {
     public ResponseEntity<List<CompanyEntity>> findCompanyByCnpj(@RequestParam(name = "cnpj", required = false) String cnpj,
                                                                  @RequestParam(name = "name", required = false) String name){
         CompanyDTO companyDTO = new CompanyDTO();
-        companyDTO.setCnpj(cnpj);
-        companyDTO.setFantasyName(name);
-        return ResponseEntity.ok(companyService.findCompanyByCnpj(companyDTO));
+        if (Objects.nonNull(name) && Objects.isNull(cnpj)){
+            companyDTO.setFantasyName(name);
+            return ResponseEntity.ok(companyService.findCompanyByCnpj(companyDTO));
+        } else if (Objects.nonNull(cnpj) && Objects.isNull(name)) {
+            companyDTO.setCnpj(cnpj);
+            return ResponseEntity.ok(companyService.findCompanyByCnpj(companyDTO));
+        }
+        return null;
     }
 
     @PostMapping
