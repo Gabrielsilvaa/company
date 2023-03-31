2d54a006e7c36d78fc1416c690edf5b0b4648bdb
Gabriel Silva Andrade <gagabrielsilva007@gmail.com>
2023-03-31 11:54:41 -0300

    ajust test

--- backend/src/test/java/com/gabriel/andrade/company_supplier/service/impl/CompanyServiceImplTest.java
+++ backend/src/test/java/com/gabriel/andrade/company_supplier/service/impl/CompanyServiceImplTest.java
@@ -79,42 +79,6 @@ class CompanyServiceImplTest {
         verify(companyRepository).findName("teste");
     }
 
-//    @Test
-//    void updateCompany() {
-//        CompanyDTO companyDTO = new CompanyDTO();
-//        companyDTO.setCep("06825025");
-//        companyDTO.setCnpj("02593356000101");
-//        companyDTO.setFantasyName("teste");
-//
-//        CompanyEntity company = new CompanyEntity();
-//
-//        given(companyRepository.findCnpj("02593356000101")).willReturn(company);
-//        CompanyEntity companyEntity = companyService.updateCompany(companyDTO);
-//
-//        verify(companyRepository).save(company);
-//        verify(companyRepository).findCnpj("02593356000101");
-//
-//    }
-
-//    @Test
-//    void saveCompany() {
-//        CompanyEntity companyEntity = new CompanyEntity();
-//        companyEntity.setCep("06825025");
-//        companyEntity.setCnpj("02593356000101");
-//        companyEntity.setFantasyName("teste");
-//
-//        CompanyDTO companyDTO = new CompanyDTO();
-//        companyDTO.setCep("06825025");
-//        companyDTO.setCnpj("02593356000101");
-//        companyDTO.setFantasyName("teste");
-//
-//        when(companyRepository.save(companyEntity)).thenReturn(companyEntity);
-//
-//        CompanyEntity created = companyService.saveCompany(companyDTO);
-//
-//        assertThat(created).isSameAs(companyEntity);
-//        verify(companyRepository).save(companyEntity);
-//    }
 
     @Test
     void deletCompany() {
